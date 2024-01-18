ec78f76a44fa0231497660be4540558b687f0d52
braxsimmons <braxsimmons01@gmail.com>
2024-01-17 23:44:56 -0700

    trying to commit the two files

---
+++ dicethrowinggame1/DiceRoller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class DiceRoller
+{
+    public int[] RollDice(int numberOfRolls)
+    {
+        // Array to store the count of each possible sum (2-12) of two dice rolls
+        int[] results = new int[11];
+        // Creating a Random object for generating random numbers
+        Random random = new Random();
+
+        // Looping for the number of rolls inputted by the user
+        for (int i = 0; i < numberOfRolls; i++)
+        {
+            // Simulating two dice rolls and summing them up
+            int roll = random.Next(1, 7) + random.Next(1, 7);
+            // Incrementing the count of the sum in array
+            results[roll - 2]++;
+        }
+
+        // Return the array of roll sums
+        return results;
+    }
+}
