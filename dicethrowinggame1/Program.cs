87ff41a9646a02c216134b0484a791f44d912fe3
braxsimmons <braxsimmons01@gmail.com>
2024-01-17 23:44:38 -0700

    split into two files for assignment

--- dicethrowinggame1/Program.cs
+++ dicethrowinggame1/Program.cs
@@ -4,43 +4,38 @@ class Program
 {
     static void Main()
     {
+        // Greeting message for the user
         Console.WriteLine("Welcome to the dice throwing simulator!");
+
+        // Prompting the user to input the number of dice rolls
         Console.Write("How many dice rolls would you like to simulate? ");
+        // Reading and parsing the user's input
         int numberOfRolls = int.Parse(Console.ReadLine());
 
+        // Creating an instance of DiceRoller
         DiceRoller roller = new DiceRoller();
+        // Calling RollDice method to simulate dice rolls
         int[] rollResults = roller.RollDice(numberOfRolls);
 
+        // Displaying the results
         Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
         Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
         Console.WriteLine($"Total number of rolls = {numberOfRolls}.");
 
+        // Looping through each possible dice roll sum
         for (int i = 0; i < rollResults.Length; i++)
         {
+            // Displaying the roll sum
             Console.Write($"{i + 2}: ");
-            int stars = rollResults[i] * 100 / numberOfRolls; // Convert to percentage
+            // Calculating the percentage
+            int stars = rollResults[i] * 100 / numberOfRolls;
+            // Displaying the *
             Console.WriteLine(new string('*', stars));
         }
 
+        // End message for the user
         Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+        // Waits for a key press before closing
         Console.ReadKey();
     }
 }
-
-public class DiceRoller
-{
-    public int[] RollDice(int numberOfRolls)
-    {
-        int[] results = new int[11]; // For storing counts of sums (2-12)
-        Random random = new Random();
-
-        for (int i = 0; i < numberOfRolls; i++)
-        {
-            int roll = random.Next(1, 7) + random.Next(1, 7); // Two dice rolls
-            results[roll - 2]++; // Increment count of the sum
-        }
-
-        return results;
-    }
-}
-
